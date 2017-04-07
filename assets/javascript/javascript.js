d72da7f62fc9a4574e80a58d87e7ea539cdffc25
[Andrew Zahler] <[andrew.zahler@gmail.com]>
2017-04-07 14:51:23 -0400

    working javascriptgit add .

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,4 +1,3 @@
-
 // ------- validate text input on page 1 ------ //
 $("#submit-btn").on("click", function() {
 
@@ -9,17 +8,12 @@ $("#submit-btn").on("click", function() {
     // var storedName = localStorage.setItem("usernameVar", userName)
 
     if (userName === '') {
-        alert ("Please enter your name!");
-        
+        alert("Please enter your name!");
+
         $(document).load("index.html");
-    }
-    
-    else {
+    } else {
 
         localStorage.setItem("name", userName);
-
-
-        
 
         // $('#name-here').html(localStorage.getItem("usernameVar")); 
     }
@@ -29,11 +23,44 @@ $("#submit-btn").on("click", function() {
 
 //******** Andrew's code begins here ********
 
+//*** variables
+// This is our main object of the top trending terms, organized by city. Each city is a child object with properties "trend" and "volume".   
+var trendingMap = {};
 
+// functions ***
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+};
 
+// 
+function objectBuilder() {
+    // init AJAX call to JSON file
+    $.ajax({
+            url: "https://api.myjson.com/bins/17jmtn",
+            method: "GET"
+        })
+        .done(function(response) {
+            for (i in response) {
+                var cityName = response[i].locations[0].name;
+                var trendName = response[i].trends[0].name;
+                var tweetVol = response[i].trends[0].tweet_volume;
+                trendingMap[cityName] = {};
+                trendingMap[cityName].trend = trendName;
+                if (tweetVol === null) {
+                    var randomTweetVol = getRandomInt(1000, 30000);
+                    trendingMap[cityName].volume = randomTweetVol;
+                } else {
+                    trendingMap[cityName].volume = tweetVol;
+                };
+            };
+            console.log(trendingMap);
+        });
+};
 
+// Main function
 
-
+objectBuilder();
 
 //******** Andrew's code ends here **********
-
